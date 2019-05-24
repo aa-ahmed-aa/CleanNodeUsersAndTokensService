3d886894e023b23c6c7b5b9b48572add87f5ad70
unknown <ahmedkhaled36@hotmail.com>
2019-05-24 20:03:48 +0200

    configs for testing and app env

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 'use strict';
 
-const express = require('express');
+const portNumber = require('./config/appConfig').port;
 const connection = require('./connection');
+const express = require('express');
 const api = require('./src/api');
 
 connection();
 
 //Init App on port 3000
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || portNumber;
 
 //Router list
 api(app);
@@ -17,3 +18,5 @@ api(app);
 app.listen(port, () => {
     console.log(`Server started on port ${port}.....`);
 });
+
+module.exports = app;
